0f43083b1b0992cd1038fd447f9d1dd547c748bd
Dannis Dayana Rincon Guevara <dannis.rincon1997@gmail.com>
2026-07-14 12:59:14 -0500

    se agrega actividad

--- utilidades.js
+++ utilidades.js
@@ -1,1 +1,26 @@
-function formatearPrecio (precio)
+function formatearPrecio (precio) {
+    return precio.toLocaleString ("es-CO");         //para colocar puntos a los numeros
+};
+
+
+function esEmailValido (email) {
+    if (email.includes ("@") && email.includes (".")) {         //aqui pregunta si el correo tiene arroba y si incluye punto (con el && debe cumplir las dos condiciones)
+        return true;
+    }
+    else {
+        return false;
+    };
+};
+
+function calcularDescuento (precio, porcentaje) {
+    let descuento = (precio * porcentaje) / 100;
+    let precioFinal = precio - descuento;
+    return precioFinal
+};
+
+
+module.exports = {
+    formatearPrecio,
+    esEmailValido,
+    calcularDescuento, 
+};
